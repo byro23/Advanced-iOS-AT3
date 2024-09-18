fad52a244f4e77f598770d9c2f5dce43dfa9361d
Byron Lester <125940003+byro23@users.noreply.github.com>
2024-09-18 11:31:02 +1000

    Initial Commit

---
+++ Advanced-iOS-AT3/Advanced_iOS_AT3App.swift
@@ -0,0 +1,17 @@
+//
+//  Advanced_iOS_AT3App.swift
+//  Advanced-iOS-AT3
+//
+//  Created by Byron Lester on 18/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct Advanced_iOS_AT3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Advanced-iOS-AT3UITests/Advanced_iOS_AT3UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Advanced_iOS_AT3UITestsLaunchTests.swift
+//  Advanced-iOS-AT3UITests
+//
+//  Created by Byron Lester on 18/9/2024.
+//
+
+import XCTest
+
+final class Advanced_iOS_AT3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
